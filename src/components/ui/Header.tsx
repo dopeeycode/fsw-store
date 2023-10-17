8baea55a5b230fc71c6ac2c82c09d0b0c3c506db
Italo <italodopeey@gmail.com>
2023-10-16 23:02:54 -0300

    feat: sheet component shadcn ui

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import { Card } from './card'
 import { Button } from './button'
 import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from './sheet'
 
 const Header = () => {
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <h1>Fodasi</h1>
+        </SheetContent>
+      </Sheet>
 
       <h1 className="font-semibold text-lg">
         <span className="text-primary">FWS</span> Store
