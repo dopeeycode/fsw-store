01cf4025c377add76257874ea55528b359fa199a
Italo <italodopeey@gmail.com>
2023-10-19 23:54:19 -0300

    chore: created constants product-icon

---
+++ src/constants/category-icon.tsx
@@ -0,0 +1,17 @@
+import {
+  HeadphonesIcon,
+  KeyboardIcon,
+  MonitorIcon,
+  MouseIcon,
+  SpeakerIcon,
+  SquareIcon,
+} from 'lucide-react'
+
+export const CATEGORY_ICON = {
+  keyboards: <KeyboardIcon size={18} />,
+  monitors: <MonitorIcon size={18} />,
+  headphones: <HeadphonesIcon size={18} />,
+  mousepads: <SquareIcon size={18} />,
+  speakers: <SpeakerIcon size={18} />,
+  mouses: <MouseIcon size={18} />,
+}
