5c206e5c7ad7b3000f4513640d16e06e49b21070
Italo <italodopeey@gmail.com>
2023-10-20 00:23:29 -0300

    refactor: viriable not used

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,6 @@ import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prismaClient } from './prisma'
 import { AuthOptions } from 'next-auth'
-import { env } from '@/env'
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
