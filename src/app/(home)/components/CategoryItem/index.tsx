14b4a2a5e28bf8aec0820c66d6249fcc362d2052
Italo <italodopeey@gmail.com>
2023-10-19 23:59:05 -0300

    refactor: add CATEGORY_ICON

--- src/app/(home)/components/CategoryItem/index.tsx
+++ src/app/(home)/components/CategoryItem/index.tsx
@@ -1,35 +1,19 @@
 import { Badge } from '@/components/ui/badge'
+import { CATEGORY_ICON } from '@/constants/category-icon'
 import { Category } from '@prisma/client'
-import {
-  HeadphonesIcon,
-  KeyboardIcon,
-  MonitorIcon,
-  MouseIcon,
-  SpeakerIcon,
-  SquareIcon,
-} from 'lucide-react'
 
 interface CategoryItemProps {
   category: Category
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-  const categoryIcon = {
-    keyboards: <KeyboardIcon size={18} />,
-    monitors: <MonitorIcon size={18} />,
-    headphones: <HeadphonesIcon size={18} />,
-    mousepads: <SquareIcon size={18} />,
-    speakers: <SpeakerIcon size={18} />,
-    mouses: <MouseIcon size={18} />,
-  }
-
   return (
     <>
       <Badge
         className="flex items-center justify-center gap-2 rounded-lg py-3 "
         variant="outline"
       >
-        {categoryIcon[category.slug as keyof typeof categoryIcon]}
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
         <span className="text-xs font-bold">{category.name}</span>
       </Badge>
     </>
