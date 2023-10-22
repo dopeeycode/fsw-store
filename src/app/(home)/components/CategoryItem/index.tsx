84a6f14d08082c98c5d90bf8ada2fe3959078e42
Italo <italodopeey@gmail.com>
2023-10-22 20:40:40 -0300

    feat: ensure user goes to category page on category item click

--- src/app/(home)/components/CategoryItem/index.tsx
+++ src/app/(home)/components/CategoryItem/index.tsx
@@ -1,6 +1,7 @@
 import { Badge } from '@/components/ui/badge'
 import { CATEGORY_ICON } from '@/constants/category-icon'
 import { Category } from '@prisma/client'
+import Link from 'next/link'
 
 interface CategoryItemProps {
   category: Category
@@ -8,7 +9,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <>
+    <Link href={`/category/${category.slug}`}>
       <Badge
         className="flex items-center justify-center gap-2 rounded-lg py-3 "
         variant="outline"
@@ -16,7 +17,7 @@ const CategoryItem = ({ category }: CategoryItemProps) => {
         {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
         <span className="text-xs font-bold">{category.name}</span>
       </Badge>
-    </>
+    </Link>
   )
 }
 
